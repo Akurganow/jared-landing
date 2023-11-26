6a224216ef99b1e2987f1491e54b1264fcad0e69
Alexander Kurganov <a.kurganow@gmail.com>
2023-11-27 00:55:12 +0400

    Refine home page

---
+++ src/components/EmojiList/index.tsx
@@ -0,0 +1,40 @@
+import React, { DetailedHTMLProps, HTMLAttributes, useMemo } from 'react'
+
+export interface EmojiListItem {
+	title: string
+	text: string
+	emoji: string | (() => string)
+}
+
+export interface EmojiListProps extends DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> {
+	items: EmojiListItem[]
+}
+
+function emojiToUnicode(input: string): string {
+	return (input.codePointAt(0) as number).toString(16)
+}
+
+const EmojiList: React.FC<EmojiListProps> = ({ items }) => {
+	const emojis = useMemo<string[]>(() =>
+		items
+			.filter(item => item.emoji)
+			.map(item => {
+				const emoji = typeof item.emoji === 'string' ? item.emoji : item.emoji()
+				return emojiToUnicode(emoji)
+			}),
+	[items]
+	)
+
+	return <>
+		<style>{
+			`@counter-style repeating-emoji { symbols: ${emojis.map(emoji => `"\\${emoji}"`).join(' ')}; suffix: " "; } .emoji-list { list-style-type: repeating-emoji; }`
+		}</style>
+		<ul className="emoji-list">
+			{items.map((item, index) => (
+				<li key={item.title+index}><strong>{item.title}:</strong> {item.text}</li>
+			))}
+		</ul>
+	</>
+}
+
+export default EmojiList
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,36 @@
 import * as React from 'react'
 import CommonHead from '../components/CommonHead'
+import EmojiList, { EmojiListItem } from '../components/EmojiList'
 import type { HeadFC, PageProps } from 'gatsby'
+
+const getCurrentHourClockEmoji = (): string => {
+	const hour = new Date().getHours()
+	const hourMod = hour % 12
+
+	switch (hourMod) {
+	case 0: return '🕛'
+	case 1: return '🕐'
+	case 2: return '🕑'
+	case 3: return '🕒'
+	case 4: return '🕓'
+	case 5: return '🕔'
+	case 6: return '🕕'
+	case 7: return '🕖'
+	case 8: return '🕗'
+	case 9: return '🕘'
+	case 10: return '🕙'
+	case 11: return '🕚'
+	default: return '🕛'
+	}
+}
+
+const items: EmojiListItem[] = [
+	{ emoji: '🖥️', title: 'Sleek Interface', text: 'Experience a refined and professional look, inspired by top-tier development environments.' },
+	{ emoji: '🔖', title: 'Bookmark Efficiency', text: 'Organize your bookmarks with precision and ease.' },
+	{ emoji: getCurrentHourClockEmoji(), title: 'History, Reorganized', text: 'Navigate your browsing history like a pro, with intuitive and specialized sections.' },
+	{ emoji: '💼', title: 'ITS & VCS Integration', text: 'Seamlessly manage your Jira, YouTrack, GitHub, and GitLab links.' },
+	{ emoji: '🧩', title: 'Customizable Layouts', text: 'Tailor your tabs to fit your workflow. It\'s your space, make it your own!' },
+]
 
 const IndexPage: React.FC<PageProps> = () => (
 	<main>
@@ -8,13 +38,9 @@ const IndexPage: React.FC<PageProps> = () => (
 		<p>🚀 <strong>Elevate Your Browsing with Elegance!</strong> 🌟</p>
 		<p>Say goodbye to the mundane new tab page. Introducing <strong>Jared</strong>, the Chrome extension that revolutionizes your browsing experience.</p>
 		<p>🔥 <strong>Features at a Glance:</strong></p>
-		<ul>
-			<li>💼 <strong>Sleek Interface</strong>: Experience a refined and professional look, inspired by top-tier development environments.</li>
-			<li>📚 <strong>Bookmark Efficiency</strong>: Organize your bookmarks with precision and ease.</li>
-			<li>🕒 <strong>History, Reorganized</strong>: Navigate your browsing history like a pro, with intuitive and specialized sections.</li>
-			<li>💼 <strong>ITS &amp; VCS Integration</strong>: Seamlessly manage your Jira, YouTrack, GitHub, and GitLab links.</li>
-			<li>🧩 <strong>Customizable Layouts</strong>: Tailor your tabs to fit your workflow. It&apos;s your space, make it your own!</li>
-		</ul>
+
+		<EmojiList items={items} />
+
 		<p>🔗 Ready to upgrade your browsing? <a href="https://chrome.google.com/webstore/detail/jared/aafbpehebcbadcpmmmkjgednfoojpifa">Get Jared now on the Chrome Web Store!</a></p>
 		<p>👨‍💻 <strong>Attention Developers!</strong> Jared is an open-source project, and we welcome contributions from the developer community. Whether it&apos;s fixing bugs, adding new features, or improving the code, your input can make a big difference.</p>
 		<p>🔗 Interested in contributing? Check out the project on <a href="https://github.com/Akurganow/jared">GitHub</a> and join our growing community of developers!</p>
