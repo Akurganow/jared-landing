9e1bad3a1cef0d1e17ee391461f19e1de781194a
Alexander Kurganov <a.kurganow@gmail.com>
2023-11-27 13:17:28 +0400

    Fix fonts

--- src/components/CommonHead/index.tsx
+++ src/components/CommonHead/index.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 export default () => <>
 	<link rel="preconnect" href="https://fonts.googleapis.com"/>
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-	<link href="https://fonts.googleapis.com/css3?family=JetBrains+Mono:wght@400;600;&display=swap" rel="stylesheet"/>
+	<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600&display=swap" rel="stylesheet"/>
 </>
