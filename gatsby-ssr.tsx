069394d3fe4460c96ddefb2e137b14bf72800f58
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-10 05:58:02 +0400

    feat: Add SEO

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,8 +1,8 @@
 import './src/styles/common.css'
 import React from 'react'
-import { GatsbyBrowser } from 'gatsby'
+import { GatsbySSR } from 'gatsby'
 import Layout from './src/components/Layout'
 
-export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element, props }) => {
+export const wrapPageElement: GatsbySSR['wrapPageElement'] = ({ element, props }) => {
 	return <Layout {...props}>{element}</Layout>
 }
