481b73d557cce160bf4b703dbc6bcc6a018dd26e
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-10 06:11:20 +0400

    chore: refactoring

--- src/utils/emoji.ts
+++ src/utils/emoji.ts
@@ -1,3 +1,5 @@
+import { EmojiListItem } from '../components/EmojiList'
+
 export const getCurrentHourClockEmoji = (): string => {
 	const hour = new Date().getHours()
 	const hour12 = hour > 11 ? hour - 12 : hour
@@ -18,3 +20,27 @@ export const getCurrentHourClockEmoji = (): string => {
 	default: return '🕛'
 	}
 }
+
+export const homePageItems: EmojiListItem[] = [
+	{
+		emoji: '🖥️',
+		title: 'Sleek Interface',
+		text: 'Experience a refined and professional look, inspired by top-tier development environments.'
+	},
+	{ emoji: '🔖', title: 'Bookmark Efficiency', text: 'Organize your bookmarks with precision and ease.' },
+	{
+		emoji: getCurrentHourClockEmoji(),
+		title: 'History, Reorganized',
+		text: 'Navigate your browsing history like a pro, with intuitive and specialized sections.'
+	},
+	{
+		emoji: '💼',
+		title: 'ITS & VCS Integration',
+		text: 'Seamlessly manage your Jira, YouTrack, GitHub, and GitLab links.'
+	},
+	{
+		emoji: '🧩',
+		title: 'Customizable Layouts',
+		text: 'Tailor your tabs to fit your workflow. It\'s your space, make it your own!'
+	},
+]
